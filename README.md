657bfa640724cce085d1b55fb969e861fced291c
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-04-14 10:16:31 -0500

    init code

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-css-transitions-loading
+css-preload
 =======================
 
-Prevent FOUC for CSS transitions.
+Prevent FOUC for CSS - e.g. for CSS3 transitions.
+
+Inspired by [http://css-tricks.com/transitions-only-after-page-load/].
+
---
+++ src/css-preload.js
@@ -0,0 +1,28 @@
+/*! MIT licensed by https://github.com/AndersDJohnson/css-preload */
+(function (root, factory) {
+  var name = 'cssPreload';
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], function ($) {
+        // Also create a global in case some scripts
+        // that are loaded still are looking for
+        // a global even when an AMD loader is in use.
+        return (root[name] = factory($));
+    });
+  } else {
+    // Browser globals
+    root[name] = factory(root.jQuery);
+  }
+}(this, function ($) {
+
+  var cssPreload = {};
+
+  cssPreload.onLoaded = function() {
+    $(document.body).removeClass("css-preload");
+  };
+
+  $(window).load(cssPreload.onLoaded);
+
+  return cssPreload;
+
+});
